0295763baa3e1e521db812359655f301ce6d6ed6
Felipe Romero <feliperomero3@outlook.com>
2020-06-09 22:43:54 -0500

    Direct the application to use the Shell

--- spa/js/spa.js
+++ spa/js/spa.js
@@ -8,11 +8,7 @@
 var spa = (function () {
   'use strict';
   var initModule = function ($container) {
-    $container.html(
-      '<h1 style="display:inline-block; margin:25px;">'
-      + 'hello world!'
-      + '</h1>'
-    );
+    spa.shell.initModule($container);
   };
 
   return { initModule: initModule };
